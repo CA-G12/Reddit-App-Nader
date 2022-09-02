e99413e40e3fda98adfb8852580305b6809f7387
nader shakshak <nadershak149@gmail.com>
2022-09-02 18:23:07 +0300

    file structure relats #1

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const { join } = require("path");
+
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+
+const router = require("./router");
+
+app.set("port", process.env.PORT || 8000);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(compression());
+app.use(cookieParser());
+app.use(express.static(join(__dirname, "..", "public")));
+// app.use(router);
+
+module.exports = app;
