739902936f52c1e8a2db0623c3a5a6bf42a14fea
nader shakshak <nadershak149@gmail.com>
2022-09-03 22:54:15 +0300

    [JS , SQL] add the user to the database with hash password #10

---
+++ server/controllers/loginPath.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const loginpath = (req, res)=>{
+    res.sendFile(path.join(__dirname, "..", "..", "public", "pages", "login.html"))
+};
+
+module.exports =loginpath;
--- server/database/config/init.sql
+++ server/database/config/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    password VARCHAR(250) NOT NULL,
     imgUrl TEXT
 );
 
