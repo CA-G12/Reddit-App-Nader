ff95b5156570eb89a2558eb2b2749158dc254350
nader shakshak <nadershak149@gmail.com>
2022-09-08 10:01:03 +0300

    verifyToken

---
+++ server/middlewares/verifyToken.js
@@ -0,0 +1,20 @@
+const { verify, decode } = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const  {dataLogin} = req.cookies;
+    if(dataLogin){
+        verify(dataLogin, process.env.SECRET_KEY,(error,decode)=>{
+            if(error){
+                res.redirect("/login");
+            }else{
+                req.decode = decode;
+                next();
+            }
+        });
+    }else{
+        res.redirect("/login");
+    }
+
+};
+
+module.exports = verifyToken;
